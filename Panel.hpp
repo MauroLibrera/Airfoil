05af545ab78af4e26c16e2697a7736695d8105e2
MauroLibrera <maurolibrera@gmail.com>
2023-03-31 21:33:02 -0300

    Inicializacion de la clase Panel

---
+++ Panel.hpp
@@ -0,0 +1,15 @@
+#ifndef PANEL_H
+#define PANEL_H
+
+namespace Airfoilspace
+{
+    class PANEL_H
+    {
+    public:
+        Vector2 PuntoI;
+        Vector2 PuntoF;
+    };
+}
+
+
+#endif // PANEL_H
