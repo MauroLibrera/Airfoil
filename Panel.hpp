ef0baa0ae43fe62aabf5d9446f30729b9de04db1
MauroLibrera <maurolibrera@gmail.com>
2023-03-31 21:56:42 -0300

    Modificado Panel

--- Panel.hpp
+++ Panel.hpp
@@ -1,13 +1,30 @@
 #ifndef PANEL_H
 #define PANEL_H
 
+#include "Vector2.hpp"
+
 namespace Airfoilspace
 {
-    class PANEL_H
+    class Panel
     {
-    public:
+    private:
         Vector2 PuntoI;
         Vector2 PuntoF;
+
+        real largo;
+        Vector2 largoComponentes;
+        Vector2 puntoCentral;
+        real anguloInclinacion;
+        int nombre;
+
+        Vector2 normalPanel;
+        real potencialVelocidad;
+        real coeficientePresion;
+
+    public:
+        Panel(const Vector2& pI, const Vector2& pF);
+        Vector2 vectoresPunto(Vector2 &k);
+
     };
 }
 
